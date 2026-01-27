282a2a725a4272e2fb9836f552d9b48e3647fdb9
nillpakhi2003-droid <nillpakhi2003@gmail.com>
2026-01-27 16:41:10 +0000

    Fix POS layout CSS - use Tailwind CDN instead of Vite

--- resources/views/pos/layout.blade.php
+++ resources/views/pos/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') | {{ __('pos.title') }}</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.tailwindcss.com"></script>
     <script>
         document.documentElement.lang = "{{ app()->getLocale() }}";
         @if(app()->getLocale() === 'bn')
