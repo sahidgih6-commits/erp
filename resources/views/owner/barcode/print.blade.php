ab989f03d744d7886b1cabfde5635f5f2cf2bf46
nillpakhi2003-droid <nillpakhi2003@gmail.com>
2026-02-02 04:25:36 +0000

    Set all paper sizes to fixed 45mm x 35mm for consistent printing

--- resources/views/owner/barcode/print.blade.php
+++ resources/views/owner/barcode/print.blade.php
@@ -5,29 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Print Barcodes - {{ $labelSize }}</title>
     <style>
-        /* Page size matching sticker dimensions */
+        /* Page size - Fixed to 45mm x 35mm for all labels */
         @page {
+            size: 45mm 35mm portrait;
             margin: 0mm;
             padding: 0mm;
-            @if($labelSize == '20x10')
-                size: 20mm 10mm portrait;
-            @elseif($labelSize == '30x20')
-                size: 30mm 20mm portrait;
-            @elseif($labelSize == '40x30')
-                size: 40mm 30mm portrait;
-            @elseif($labelSize == '45x35')
-                size: 45mm 35mm portrait;
-            @elseif($labelSize == '50x30')
-                size: 50mm 30mm portrait;
-            @elseif($labelSize == '60x40')
-                size: 60mm 40mm portrait;
-            @elseif($labelSize == '70x50')
-                size: 70mm 50mm portrait;
-            @elseif($labelSize == '100x50')
-                size: 100mm 50mm portrait;
-            @else
-                size: 45mm 35mm portrait;
-            @endif
         }
         
         * {
